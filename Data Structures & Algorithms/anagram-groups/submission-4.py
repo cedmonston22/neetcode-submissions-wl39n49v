fb01afbe028b58614b7966160f0b3ff010be42f2
Cole Edmonston <cedmonst@calpoly.edu>
2026-06-02 11:28:44 -0700

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.py
@@ -0,0 +1,14 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        #create a hashmap to store the groups of anagrams
+        #we we will iterate through the list, sort the word
+        #see if the sorted word matches any of the hashmap keys if so add as a value else create a new key
+        #return the values in the form of an array
+        anagrams = defaultdict(list)
+        res = []
+        for i in strs:
+            anagrams["".join(sorted(i))].append(i)
+        for value in anagrams.values():
+            res.append(value)
+        
+        return res
